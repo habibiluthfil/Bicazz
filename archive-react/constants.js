0393486ed5c5231d27349ef5917def38190264de
habibiluthfil <habibi.lutfhil@gmail.com>
2026-04-12 18:36:23 +0700

    remodel

--- archive-react/constants.js
+++
@@ -1,5 +0,0 @@
-// utils/constants.js
-// File ini berisi konstanta-konstanta yang digunakan di seluruh aplikasi Bicazz.
-// Fungsi: Menyimpan nilai-nilai tetap seperti URL logo untuk memudahkan pemeliharaan dan perubahan.
-
-export const LOGO_URL = "https://files.oaiusercontent.com/file-K1S2oXk7Y8mN5R5z2T9w2S";
